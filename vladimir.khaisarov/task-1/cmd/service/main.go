d70cf62385aec965ca98e70f82ca088f32e03720
xaysee <vovaah021007@gmail.com>
2026-09-19 15:46:41 +0300

    [TASK-1] Fix calculator logic

--- vladimir.khaisarov/task-1/cmd/service/main.go
+++ vladimir.khaisarov/task-1/cmd/service/main.go
@@ -7,37 +7,43 @@ func main() {
 	var iNumber2 int
 	var operation string
 	fmt.Println("enter first number: ")
-	_, err := fmt.Scanln(&iNumber1)
+	_,err := fmt.Scanln(&iNumber1)
 	if err != nil {
 		fmt.Println("ERROR: not a number")
 		return
 	}
 	fmt.Println("enter second number: ")
-	_, err = fmt.Scanln(&iNumber2)
+	_,err = fmt.Scanln(&iNumber2)
 	if err != nil {
 		fmt.Println("ERROR: not a number")
 		return
 	}
 	fmt.Println("choose the operation (+, -, *, /): ")
 	fmt.Scanln(&operation)
+	_,err = fmt.Scanln(&operation)
+	if err != nil {
+		fmt.Println("ERROR: invalid operation")
+		return
+	}
 
-	if operation == "+" {
+	switch operation {
+	case "+":
 		fmt.Println("result: ", iNumber1+iNumber2)
-	}
-	if operation == "-" {
-		fmt.Println("result: ", iNumber1+iNumber2)
-	}
-	if operation == "*" {
+
+	case "-":
+		fmt.Println("result: ", iNumber1-iNumber2)
+
+	case "*":
 		fmt.Println("result: ", iNumber1*iNumber2)
-	}
-	if operation == "/" {
+
+	case "/":
 		if iNumber2 == 0 {
 			fmt.Println("ERROR: divizion by zero")
 			return
 		}
+
 		fmt.Println("result: ", iNumber1/iNumber2)
-	} else {
+	default:
 		fmt.Println("wrong operation")
 	}
-
 }
