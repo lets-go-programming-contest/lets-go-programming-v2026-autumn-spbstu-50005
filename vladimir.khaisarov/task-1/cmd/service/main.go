da51c8417e975e011fc892cec4ed155200749a90
xaysee <vovaah021007@gmail.com>
2026-09-19 16:26:11 +0300

    [TASK-1] Repaired code

--- vladimir.khaisarov/task-1/cmd/service/main.go
+++ vladimir.khaisarov/task-1/cmd/service/main.go
@@ -3,24 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	var iNumber1 int
-	var iNumber2 int
+	var iNumber1, iNumber2 int
 	var operation string
 	fmt.Println("enter first number: ")
-	_,err := fmt.Scanln(&iNumber1)
+	_, err := fmt.Scanln(&iNumber1)
 	if err != nil {
 		fmt.Println("ERROR: not a number")
 		return
 	}
 	fmt.Println("enter second number: ")
-	_,err = fmt.Scanln(&iNumber2)
+	_, err = fmt.Scanln(&iNumber2)
 	if err != nil {
 		fmt.Println("ERROR: not a number")
 		return
 	}
 	fmt.Println("choose the operation (+, -, *, /): ")
-	fmt.Scanln(&operation)
-	_,err = fmt.Scanln(&operation)
+	_, err = fmt.Scanln(&operation)
 	if err != nil {
 		fmt.Println("ERROR: invalid operation")
 		return
@@ -28,13 +26,13 @@ func main() {
 
 	switch operation {
 	case "+":
-		fmt.Println("result: ", iNumber1+iNumber2)
+		fmt.Println(iNumber1 + iNumber2)
 
 	case "-":
-		fmt.Println("result: ", iNumber1-iNumber2)
+		fmt.Println(iNumber1 - iNumber2)
 
 	case "*":
-		fmt.Println("result: ", iNumber1*iNumber2)
+		fmt.Println(iNumber1 * iNumber2)
 
 	case "/":
 		if iNumber2 == 0 {
@@ -42,7 +40,7 @@ func main() {
 			return
 		}
 
-		fmt.Println("result: ", iNumber1/iNumber2)
+		fmt.Println(iNumber1 / iNumber2)
 	default:
 		fmt.Println("wrong operation")
 	}
