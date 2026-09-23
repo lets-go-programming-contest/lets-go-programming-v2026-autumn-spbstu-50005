d41ebe49ff83e570033593bcec989dc4bae738e0
sidorenkoanastasia-ca <sidorenkoanastasia.ca@gmail.com>
2026-09-23 20:14:16 +0300

    [TASK-1] Fix error warnings check of Scanln

--- anastasia.sidorenko/task-1/cmd/service/main.go
+++ anastasia.sidorenko/task-1/cmd/service/main.go
@@ -7,9 +7,18 @@ import (
 
 func main() {
 	var firstOp, secondOp, op string
-	fmt.Scanln(&firstOp)
-	fmt.Scanln(&secondOp)
-	fmt.Scanln(&op)
+	if _, err := fmt.Scanln(&firstOp); err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	if _, err := fmt.Scanln(&secondOp); err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	if _, err := fmt.Scanln(&op); err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
 	firstNumber, err := strconv.Atoi(firstOp)
 	if err != nil {
 		fmt.Println("Invalid first operand")
